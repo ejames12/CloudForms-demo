0dce81fe907fb8422719ecdf33aad2732dcc01c4
ejames12 <erwan@nuagenetworks.net>
2018-04-17 14:52:40 -0400

    Update create_neutron_net.sh

--- create_neutron_net.sh
+++ create_neutron_net.sh
@@ -2,4 +2,4 @@
 
 nuagenet=$1
 
-sudo sshpass -p Alcateldc ssh -oStrictHostKeyChecking=no root@10.0.0.4 "neutron net-create testnet1 | neutron subnet-create testnet1 192.168.3.0/24 --name testsubnet1 --nuagenet $nuagenet --net-partition 'Company XYZ'"
+ssh -oStrictHostKeyChecking=no root@10.0.0.4 "neutron net-create testnet1 | neutron subnet-create testnet1 192.168.3.0/24 --name testsubnet1 --nuagenet $nuagenet --net-partition 'Company XYZ'"
