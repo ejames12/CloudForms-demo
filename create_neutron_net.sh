f03599a10f539295a14f711f02c7a4018b816529
ejames12 <erwan@nuagenetworks.net>
2018-04-17 14:15:27 -0400

    Create create_neutron_net.sh

---
+++ create_neutron_net.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+nuagenet=$1
+
+ssh 10.0.0.4 "neutron net-create testnet1 | neutron subnet-create testnet1 192.168.3.0/24 --name testsubnet1 --nuagenet $nuagenet --net-partition 'Company XYZ'"
